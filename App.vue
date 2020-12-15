a2bc8f9e0087b7da24b7a2ff4fa55d109828dc6f
Jin-longwu <861237702@qq.com>
2020-12-15 08:09:32 +0800

    修改了

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	page{
+		height: 100%;
+		background-color: #f9f9f9;
+	}
+</style>
